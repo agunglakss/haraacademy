01c0da870553ba3526d693594bbe8a576725cbd8
Agung Laksono <agunglakss@gmail.com>
2022-08-20 03:17:42 +0700

    add page about-us and contact-us

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+  get 'about', to: 'about#index'
+  get 'contact', to: 'contact#index'
 
   devise_for :users, controllers: { passwords: 'passwords' }
   get 'admins', to: 'admins#index'
