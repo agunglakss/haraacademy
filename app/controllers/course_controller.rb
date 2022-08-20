1acd3877828d864baf3df4bf7495312a1923891e
Agung Laksono <agunglakss@gmail.com>
2022-08-20 16:43:10 +0700

    add feat: find by category

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,7 +1,16 @@
 class CourseController < ApplicationController
   def index
-    @categories = Category.all
-    @speakers = Speaker.all
-    @courses = Course.all
+    if params[:category].nil? || params[:category] == ""
+      @courses = Course.all
+      @total_course = Course.count
+    else
+      category = Category.where(name: params[:category]).take
+      if category.nil?
+        redirect_to courses_path
+      end
+      
+      @courses = Course.where(category_id: category)
+      @total_course = Course.where(category_id: category).count
+    end
   end
 end
