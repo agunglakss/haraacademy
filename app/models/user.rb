654aea99aa85d1ef0d616c5e05ce371d9ab089a9
Agung Laksono <agunglakss@gmail.com>
2022-08-30 18:52:43 +0700

    add page admin users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  enum :role, [:member, :admin]
+
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
