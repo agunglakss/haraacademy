8a9e006f5351900a3581938d28d565850a11a8bf
Agung Laksono <agunglakss@gmail.com>
2022-09-06 19:50:19 +0700

    change size thumbnail

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @courses_playback = Course.includes(:category, :speaker).by_status_and_type('playback').order(:created_at).limit(4)
-    @courses_live = Course.includes(:category, :speaker).by_status_and_type('live').order(:created_at).limit(4)
-    @categories = Category.select(:name).order(sequence: :asc).take(3)
-  end
-
-  def format_number(number)
-    num_groups = number.to_s.chars.to_a.reverse.each_slice(3)
-    num_groups.map(&:join).join(',').reverse
+    @course = Course.includes(:category, :speaker).take
+    # @courses_live = Course.includes(:category, :speaker).by_status_and_type('live').order(:created_at).limit(4)
+    # @categories = Category.select(:name).order(sequence: :asc).take(3)
   end
 end
