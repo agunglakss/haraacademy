75831b6d7887b919d36b7f603f87e8ae1a84b380
Agung Laksono <agunglakss@gmail.com>
2022-09-07 12:42:29 +0700

    add gemfile midtrans

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ end
 
 gem 'devise', '~> 4.2'
 gem 'kaminari'
+gem 'veritrans'
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+
   def show
     @course = Course.where(id: params[:id]).take
     if @course.nil? || current_user.nil?
@@ -16,16 +17,41 @@ class OrdersController < ApplicationController
   end
 
   def create
+    require 'veritrans'
     course = Course.where(id: params[:id]).take
-
+    @order = Order.new 
     if course && !current_user.nil?
-        order = Order.new 
-        order.status = 'unpaid'
-        order.course_id = course.id
-        order.user_id = current_user.id
-        order.save
+        
+        @order.status = 'unpaid'
+        @order.course_id = course.id
+        @order.user_id = current_user.id
+        
     end
-    
-    
+
+    if @order.save
+      mt_client = Midtrans.new(
+      server_key: "SB-Mid-server-t_7OHErrySsJ4HF_M9kUngkC",
+      client_key: "SB-Mid-client-ouyDCkaQENXIAQjT",
+      api_host: "https://api.sandbox.midtrans.com", # default
+      http_options: { }, # optional
+      logger: Logger.new(STDOUT), # optional
+      file_logger: Logger.new(STDOUT), # optional
+    )
+    result = mt_client.create_snap_redirect_url(
+      transaction_details: {
+        order_id: @order.id,
+        gross_amount: course.price,
+        secure: true
+      },
+      item_details: {
+        id: course.id,
+        price: course.price,
+        name: course.title,
+        quantity: 1
+      }
+    )
+  end
+  
+  redirect_to result.redirect_url, allow_other_host: true
   end
 end
