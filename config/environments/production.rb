5e529439df3b19d415bbc0940d6babe63438056a
Agung Laksono <agunglakss@gmail.com>
2022-09-11 21:31:00 +0700

    fix url asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = "34.101.210.134"
+  config.asset_host = "http://haraacademy.id"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
